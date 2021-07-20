e2054e33279b87aa6c98780edc011f34d486ffc3
discretegames <discretizedgames@gmail.com>
2021-07-20 14:28:58 -0700

    Improved gapped sorter with randomness but now tests are getting slow :/

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -5,6 +5,7 @@ import unittest
 import DSAs.sorting as sorting
 from itertools import product
 
+thoroughness = 5  # Higher is more thorough but exponentially slower. Keep at 4 minimum.
 test_maps = (bool,
              float,
              lambda x: x,
@@ -35,19 +36,21 @@ def sorter_test(sorter, stable, in_place):
 
 
 # todo make sure some gaps fail as well, like [2],
-def gapped_sorter_test(sorter, stable, gaps_list):
+def gapped_sorter_test(sorter, stable, good_gaps, bad_gaps):
     def tester(self):
-        def sorter_with_gaps(gaps):
+        def call_gapped_sorter(gaps):
             def gapped_sorter(arr, key=None, reverse=False):
                 sorter(arr, key=key, reverse=reverse, gaps=gaps)
-            return gapped_sorter
-        for gaps in gaps_list:
-            sorter_test(sorter_with_gaps(gaps), stable, True)(self)
+            sorter_test(gapped_sorter, stable, True)(self)
+
+        for gaps in good_gaps:
+            call_gapped_sorter(gaps)
+        for gaps in bad_gaps:
+            self.assertRaises(AssertionError, call_gapped_sorter, gaps)
     return tester
 
 
 class TestSorting(unittest.TestCase):
-
     def confirm_sorted(self, nip_sorter, arr, key=None, reverse=False):
         expected = sorted(arr, key=key, reverse=reverse)
         actual = nip_sorter(arr, key=key, reverse=reverse)
@@ -89,6 +92,7 @@ class TestSorting(unittest.TestCase):
                         nonlocal failed
                         failed = True
             self.thorough_sort_tests(nip_sorter, maps, keys, confirmer)
+            self.random_sort_tests(nip_sorter, maps, keys, confirmer)
             self.assertTrue(failed, "No instability found. The sort may be stable.")
 
     def basic_sort_tests(self, nip_sorter):  # Doesn't check stability.
@@ -102,24 +106,26 @@ class TestSorting(unittest.TestCase):
         self.assertEqual(nip_sorter([1, 2, 3, 4]), [1, 2, 3, 4])
         self.assertEqual(nip_sorter([4, 3, 2, 1]), [1, 2, 3, 4])
 
-    def thorough_sort_tests(self, nip_sorter, maps=test_maps, keys=test_keys, confirmer=None, max_length=5):
+    def thorough_sort_tests(self, nip_sorter, maps=test_maps, keys=test_keys, confirmer=None):
         if not confirmer:
             confirmer = self.confirm_sorted
-        for length, map_, key, reverse in product(range(max_length + 1), maps, keys, [False, True]):
+        for length, map_, key, reverse in product(range(thoroughness + 1), maps, keys, (False, True)):
             for arr in product(range(length), repeat=length):
                 confirmer(nip_sorter, list(map(map_, arr)), key=key, reverse=reverse)
 
-    def random_sort_tests(self, nip_sorter):
-        def random_tests(min_length, max_length, min_value, max_value, times=10):
-            for _ in range(times):
-                arr = tutil.random_arr(min_length, max_length, min_value, max_value)
-                self.confirm_sorted(nip_sorter, arr)
-                self.confirm_sorted(nip_sorter, arr, reverse=True)
+    def random_sort_tests(self, nip_sorter, maps=(lambda x: x,), keys=(None,), confirmer=None):
+        if not confirmer:
+            confirmer = self.confirm_sorted
+
+        def random_tests(min_len, max_len, min_val, max_val, times=10):
+            for map_, key, reverse, _ in product(maps, keys, (False, True), range(times)):
+                arr = tutil.random_arr(min_len, max_len, min_val, max_val)
+                confirmer(nip_sorter, list(map(map_, arr)), key=key, reverse=reverse)
 
         random_tests(0, 10, 0, 100, 20)
-        random_tests(0, 100, -20, 20, 20)
-        random_tests(500, 1000, -10, 10, 5)
-        random_tests(500, 1000, -10**9, 10**9, 5)
+        random_tests(0, 100, 0, 10, 20)
+        random_tests(200, 500, -10, 10, 5)
+        random_tests(200, 500, -10**9, 10**9, 5)
 
     def test_decorators(self):
         def confirm_properties(sorter, stable, in_place):
@@ -140,7 +146,6 @@ class TestSorting(unittest.TestCase):
                 stabilized = sorting.not_in_place(stabilized)
             self.confirm_stability(stabilized, True)
 
-        # todo unpythonic?
         stable_ip_sorter = lambda arr, key=None, reverse=False: arr.sort(key=key, reverse=reverse)
         stable_nip_sorter = sorted
         unstable_ip_sorter = sorting.selectionsort
@@ -155,8 +160,8 @@ class TestSorting(unittest.TestCase):
     test_cocktailsort = sorter_test(sorting.cocktailsort, True, True)
     test_insertionsort = sorter_test(sorting.insertionsort, True, True)
     test_selectionsort = sorter_test(sorting.selectionsort, False, True)
-    test_shellsort = gapped_sorter_test(sorting.shellsort, False, [None, [2, 1], [2, 1, 2]])
-    test_stable_shellsort = gapped_sorter_test(sorting.shellsort, True, [[1], [1, 2]])
+    test_shellsort = gapped_sorter_test(sorting.shellsort, False, (None, [2, 1], [2, 1, 2], [100, 1]), ([2], [100]))
+    test_stable_shellsort = gapped_sorter_test(sorting.shellsort, True, ([1], [1, 2]), ())
 
 
 if __name__ == "__main__":
