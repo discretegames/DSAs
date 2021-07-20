4ae29f881dfae0996d48a211ae6eb6aa24c7e3d0
discretegames <discretizedgames@gmail.com>
2021-07-20 08:09:15 -0700

    Full stability tests working.

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -2,25 +2,17 @@
 
 import tutil  # Must import tutil before DSAs for VSCode debugging to work.
 import DSAs.sorting as sorting
-from itertools import permutations, product
+from itertools import combinations_with_replacement, permutations, product
 import unittest
 
-test_maps = (int, float, bool,
+test_maps = (bool, float,
              lambda x: x,
-             lambda x: 0,
              lambda x: -x,
              lambda x: x // 2,
-             lambda x: x + 100,
-             lambda x: x - 100,
              lambda x: x**2,
-             lambda x: 7e+9 * x,
-             lambda x: 7e-9 * x)
+             lambda _: 0)
 test_keys = (None, str,
-             lambda x: [x],
-             lambda x: [x, x - 1],
-             lambda x: (x,),
              lambda x: (x + 1, x),
-             lambda x: f"No. {x}",
              lambda x: str(x)[::-1]
              ) + test_maps
 
@@ -53,18 +45,36 @@ def sorter_test(sorter, stable=True, in_place=True):
 
 class TestSorting(unittest.TestCase):
 
-    def confirm_stability(self, nip_sorter, stable):
-        def make_map(f):
-            incrementer = 0
+    def confirm_sorted(self, nip_sorter, arr, key=None, reverse=False):
+        expected = sorted(arr, key=key, reverse=reverse)
+        actual = nip_sorter(arr, key=key, reverse=reverse)
+        self.assertEqual(actual, expected)
 
-            def mapper(x):
-                nonlocal incrementer
-                incrementer += 1
-                return f(x), incrementer
-            return mapper
-        maps = lambda x: 0, lambda x: x // 2, lambda x: x // 3
-        assertion = all if stable else lambda data: False in data
-        self.thorough_sort_tests(nip_sorter, map(make_map, maps), [lambda x: x[0]], assertion, 6)
+    def confirm_stability(self, nip_sorter, stable):
+        incrementer = 0
+
+        def mapper(value):
+            nonlocal incrementer
+            incrementer += 1
+            return value, incrementer
+        maps, keys = [mapper], [lambda x: x[0]]
+
+        if stable:
+            self.thorough_sort_tests(nip_sorter, maps, keys)
+        else:
+            failed = False
+
+            def confirmer(nip_sorter, arr, key=None, reverse=False):
+                if len(set(map(key, arr))) == len(arr):
+                    self.confirm_sorted(nip_sorter, arr, key, reverse)
+                else:
+                    expected = sorted(arr, key=key, reverse=reverse)
+                    actual = nip_sorter(arr, key=key, reverse=reverse)
+                    if expected != actual:  # When unstable some failures should happen.
+                        nonlocal failed
+                        failed = True
+            self.thorough_sort_tests(nip_sorter, maps, keys, confirmer)
+            self.assertTrue(failed, "No instability found. The sort may be stable.")
 
     def confirm_in_placeness(self, stable_sorter, in_place):
         pass  # todo
@@ -80,29 +90,24 @@ class TestSorting(unittest.TestCase):
         self.assertEqual(nip_sorter([1, 2, 3, 4]), [1, 2, 3, 4])
         self.assertEqual(nip_sorter([4, 3, 2, 1]), [1, 2, 3, 4])
 
-    def get_sort_checker(self, nip_sorter):
-        def checker(arr, key=None, reverse=False):
-            expected = sorted(arr, key=key, reverse=reverse)
-            actual = nip_sorter(arr, key=key, reverse=reverse)
-            return actual == expected
-        return checker
+    def thorough_sort_tests(self, nip_sorter, maps=test_maps, keys=test_keys, confirmer=None, max_length=5):
+        if not confirmer:
+            confirmer = self.confirm_sorted
 
-    # todo rewrite so checker asserts sorted for better error msgs
-    def thorough_sort_tests(self, nip_sorter, maps=test_maps, keys=test_keys, assertion=all, max_length=5):
-        checker = self.get_sort_checker(nip_sorter)
-        results = []
-        for length, map_, key, reverse in product(range(max_length + 1), maps, keys, [False, True]):
-            for perm in permutations(range(length), length):
-                arr = list(map(map_, perm))
-                results.append(checker(arr, key=key, reverse=reverse))
-        self.assertTrue(assertion(results))
+        arrays = set()
+        for length in range(max_length + 1):
+            for combo in combinations_with_replacement(range(length), length):
+                for perm in permutations(combo):  # Probably a better way to get all permutations here.
+                    arrays.add(perm)
+        for arr, map_, key, reverse in product(arrays, maps, keys, [False, True]):
+            confirmer(nip_sorter, list(map(map_, arr)), key=key, reverse=reverse)
 
     def random_sort_tests(self, nip_sorter):
-        checker = self.get_sort_checker(nip_sorter)
-
         def random_tests(min_length, max_length, min_value, max_value, times=10):
             for _ in range(times):
-                self.assertTrue(checker(tutil.random_arr(min_length, max_length, min_value, max_value)))
+                arr = tutil.random_arr(min_length, max_length, min_value, max_value)
+                self.confirm_sorted(nip_sorter, arr)
+                self.confirm_sorted(nip_sorter, arr, reverse=True)
 
         random_tests(0, 10, 0, 100, 20)
         random_tests(0, 100, -20, 20, 20)
