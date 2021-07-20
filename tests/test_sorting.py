b6d8a4860448e0eb806dbca1ff17b0a7805e5dde
discretegames <discretizedgames@gmail.com>
2021-07-20 09:22:14 -0700

    Decorators tests written and working.

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,45 +1,36 @@
 """Test suite for DSAs.sorting."""
 
 import tutil  # Must import tutil before DSAs for VSCode debugging to work.
+import unittest
 import DSAs.sorting as sorting
 from itertools import combinations_with_replacement, permutations, product
-import unittest
 
-test_maps = (bool, float,
+test_maps = (bool,
+             float,
              lambda x: x,
              lambda x: -x,
              lambda x: x // 2,
              lambda x: x**2,
              lambda _: 0)
-test_keys = (None, str,
+test_keys = (None,
+             str,
              lambda x: (x + 1, x),
              lambda x: str(x)[::-1]
              ) + test_maps
 
 
-def sorter_test(sorter, stable=True, in_place=True):
+def sorter_test(sorter, stable, in_place):
     def tester(self):
-        nip_sorter = sorter if not in_place else sorting.not_in_place(sorter)
-
-        self.confirm_stability(nip_sorter, stable)
-
-        stable_sorter = sorter if stable else sorting.stabilize(in_place)(sorter)
-
-        # else:
-        #     self.confirm_unstable(sorter)
-        #     sorter = sorting.util.stabilize(sorter)
-
-        # if in_place:
-        #     self.confirm_in_place(sorter)
-        # else:
-        #     self.confirm_not_in_place(sorter)
-        #     sorter = sorting.util.in_place(sorter)
-
-        snip_sorter = nip_sorter if stable else sorting.stabilize(False)(nip_sorter)
-
-        self.basic_sort_tests(snip_sorter)
-        self.thorough_sort_tests(snip_sorter)
-        self.random_sort_tests(snip_sorter)
+        nonlocal sorter
+        self.confirm_in_placeness(sorter, in_place)
+        if in_place:
+            sorter = sorting.not_in_place(sorter)
+        self.confirm_stability(sorter, stable)
+        if not stable:
+            sorter = sorting.stabilize(False)(sorter)
+        self.basic_sort_tests(sorter)
+        self.thorough_sort_tests(sorter)
+        self.random_sort_tests(sorter)
     return tester
 
 
@@ -49,6 +40,18 @@ class TestSorting(unittest.TestCase):
         expected = sorted(arr, key=key, reverse=reverse)
         actual = nip_sorter(arr, key=key, reverse=reverse)
         self.assertEqual(actual, expected)
+
+    def confirm_in_placeness(self, sorter, in_place):
+        arr = [2, 5, 4, 1, 3]
+        if in_place:
+            sorter(arr)
+            sorted_arr = arr
+        else:
+            copy = arr.copy()
+            sorted_arr = sorter(arr)
+            self.assertEqual(copy, arr)
+
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
 
     def confirm_stability(self, nip_sorter, stable):
         incrementer = 0
@@ -75,9 +78,6 @@ class TestSorting(unittest.TestCase):
                         failed = True
             self.thorough_sort_tests(nip_sorter, maps, keys, confirmer)
             self.assertTrue(failed, "No instability found. The sort may be stable.")
-
-    def confirm_in_placeness(self, stable_sorter, in_place):
-        pass  # todo
 
     def basic_sort_tests(self, nip_sorter):  # Doesn't check stability.
         self.assertEqual(nip_sorter([]), [])
@@ -114,15 +114,41 @@ class TestSorting(unittest.TestCase):
         random_tests(500, 1000, -10, 10, 5)
         random_tests(500, 1000, -10**9, 10**9, 5)
 
-    test_bubblesort = sorter_test(sorting.bubblesort)
-    test_selectionsort = sorter_test(sorting.selectionsort, False)
+    def test_decorators(self):
+        def confirm_properties(sorter, stable, in_place):
+            self.confirm_in_placeness(sorter, in_place)
+            if in_place:
+                nip = sorting.not_in_place(sorter)
+                self.confirm_in_placeness(nip, False)
+                self.confirm_in_placeness(sorting.in_place(nip), True)
+                self.confirm_stability(nip, stable)
+            else:
+                ip = sorting.in_place(sorter)
+                self.confirm_in_placeness(ip, True)
+                self.confirm_in_placeness(sorting.not_in_place(ip), False)
+                self.confirm_stability(sorter, stable)
 
-    # todo test decorators
-    # def test_decorators(self):
-    #     stable_sort = sorted
+            stabilized = sorting.stabilize(in_place)(sorter)
+            if in_place:
+                stabilized = sorting.not_in_place(stabilized)
+            self.confirm_stability(stabilized, True)
+
+        stable_ip_sorter = lambda arr, key=None, reverse=False: arr.sort(key=key, reverse=reverse)
+        stable_nip_sorter = sorted
+        unstable_ip_sorter = sorting.selectionsort
+        unstable_nip_sorter = sorting.not_in_place(unstable_ip_sorter)
+
+        confirm_properties(stable_ip_sorter, True, True)
+        confirm_properties(stable_nip_sorter, True, False)
+        confirm_properties(unstable_ip_sorter, False, True)
+        confirm_properties(unstable_nip_sorter, False, False)
+
+    test_bubblesort = sorter_test(sorting.bubblesort, True, True)
+    test_selectionsort = sorter_test(sorting.selectionsort, False, True)
 
 
 if __name__ == "__main__":
+    print('Running from main...')
     try:
         unittest.main()
     except SystemExit:  # So debugger doesn't trigger.
